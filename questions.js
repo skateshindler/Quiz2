d5031fe4f2b9e42ee60075053cc595daa4ffe14c
skateshindler <57732073+skateshindler@users.noreply.github.com>
2020-01-20 00:18:11 -0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,38 @@
+var questions = [
+  {
+    title: "How many players are on a hockey team?",
+    choices: ["300", "none", "20", "22"],
+    answer: "20"
+  },
+  {
+    title: "Who stops the puck?",
+    choices: ["goalie", "referee", "crowd", "center"],
+    answer: "goalie"
+  },
+  {
+    title: "How many players on the ice at a time?",
+    choices: ["16", "6", "4", "100"],
+    answer: "6"
+  },
+  {
+    title: "What team is LA's team?",
+    choices: ["Ducks", "Kings", "Canucks", "UCLA"],
+    answer: "Kings"
+  },
+  {
+    title: "What do you hit in hockey to score a goal?",
+    choices: ["ball", "puck", "ring", "shoe"],
+    answer: "puck"
+  },
+  {
+    title: "How many letters are in hockey?",
+    choices: ["0", "13", "6", "1"],
+    answer: "6"
+  },
+  {
+    title: "Do players wear protective gear?",
+    choices: ["yes", "no", "maybe", "so"],
+    answer: "yes"
+  }
+]
+
